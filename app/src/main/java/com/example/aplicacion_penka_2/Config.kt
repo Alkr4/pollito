be8e234bb684378a9a96215a9bcc4705f61d84c5
Alkr4 <cris.al.lat.bar@gmail.com>
2025-11-24 12:37:39 -0300

    ola

--- app/src/main/java/com/example/aplicacion_penka_2/Config.kt
+++ app/src/main/java/com/example/aplicacion_penka_2/Config.kt
@@ -1,6 +1,6 @@
 package com.example.aplicacion_penka_2
 
 object Config {
-    private const val IP = "192.168.1.11"
+    private const val IP = "10.60.236.92"
     const val URL_BASE = "http://$IP/dogwater/AppMovil/"
 }
